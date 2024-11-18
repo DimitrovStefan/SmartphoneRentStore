fb4bb354f9f5f24cfdb61a250585c624034672ea
stefan9764 <stefan100297@gmail.com>
2024-11-18 10:57:16 +0200

    BecomeSupplierFormModel created

--- SmartphoneRentStore.Core/Models/Supplier/BecomeSupplierFormModel.cs
+++ SmartphoneRentStore.Core/Models/Supplier/BecomeSupplierFormModel.cs
@@ -1,6 +1,24 @@
 ﻿namespace SmartphoneRentStore.Core.Models.Supplier
 {
+    using System.ComponentModel.DataAnnotations;
+    using static SmartphoneRentStore.Core.Constants.MessageConstants;
+    using static SmartphoneRentStore.Infrastructure.Constants.DataConstants;
+
     public class BecomeSupplierFormModel
     {
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(SupplierPhoneMaxLength,
+            MinimumLength = SupplierPhoneMinLength,
+            ErrorMessage = LengthMessage)]
+        public string PhoneNumber { get; set; } = null!;
+        [Display(Name = "Phone number")]
+        [Phone()]
+
+        
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(SupplierCityMaxLength, 
+            MinimumLength = SupplierCityMinLength,
+            ErrorMessage = LengthMessage)]
+        public string City { get; set; } = null!;
     }
 }
