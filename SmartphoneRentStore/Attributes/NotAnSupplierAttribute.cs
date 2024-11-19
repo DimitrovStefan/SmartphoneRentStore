805c99abb3a6c715b7402cbbaff5ab7f614c6797
stefan9764 <stefan100297@gmail.com>
2024-11-19 11:31:05 +0200

    NotAnSupplier attribure created

---
+++ SmartphoneRentStore/Attributes/NotAnSupplierAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace SmartphoneRentStore.Attributes
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using SmartphoneRentStore.Core.Contracts;
+    using SmartphoneRentStore.Extensions;
+
+    public class NotAnSupplierAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            
+            ISupplierService? supplierService = context.HttpContext.RequestServices.GetService<ISupplierService>();
+
+            if (supplierService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError); 
+            }
+
+            if (supplierService != null && supplierService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+
+        }
+    }
+}
