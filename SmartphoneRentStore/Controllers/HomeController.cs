888d3c992ebe54a3873813a25009d03c18839a6c
stefan9764 <stefan100297@gmail.com>
2024-11-28 16:43:55 +0200

    Error 400, ,401, 404 in production added own images

--- SmartphoneRentStore/Controllers/HomeController.cs
+++ SmartphoneRentStore/Controllers/HomeController.cs
@@ -32,9 +32,25 @@ namespace SmartphoneRentStore.Controllers
 
         [AllowAnonymous] // don't need logg in 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            { 
+                return View("Error400");
+            }
+
+            if (statusCode == 400)
+            {
+                return View("Error401");
+            }
+
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+
+
+            return View();
         }
     }
 }
