4aece46b686680b8385fbb96927adf940f662217
stefan9764 <stefan100297@gmail.com>
2024-11-28 16:17:47 +0200

    Added own exeption

---
+++ SmartphoneRentStore.Core/Exeptions/UnauthorizedActionExeption.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartphoneRentStore.Core.Exeptions
+{
+    using System;
+
+    public class UnauthorizedActionExeption : Exception // Creating own exeption
+    {
+        public UnauthorizedActionExeption()
+        {
+                
+        }
+
+        public UnauthorizedActionExeption(string message) 
+            : base(message)
+        {
+            
+        }
+    }
+}
